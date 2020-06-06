614ed9352afc83101b0f1edace293048801a9d49
PJ Hoberman <pj@twoparts.com>
2020-06-05 21:28:53 -0600

    moved articles under proper directory

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("ginger_arxiv.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("articles/", include("ginger_arxiv.articles.urls", namespace="articles")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
