7090f93ddf24e95094fe9993b87f9726bd9a8c88
PJ Hoberman <pj@twoparts.com>
2020-06-06 12:06:39 -0600

    Set up page listing all authors

--- ginger_arxiv/articles/urls.py
+++ ginger_arxiv/articles/urls.py
@@ -5,5 +5,6 @@ from ginger_arxiv.articles import views
 app_name = "articles"
 urlpatterns = [
     path("", views.list_articles, name="list_articles"),
-    path("author/<int:author_pk>", views.author_detail, name="author_detail"),
+    path("authors", views.author_list, name="author_list"),
+    path("authors/<int:author_pk>", views.author_detail, name="author_detail"),
 ]
--- ginger_arxiv/articles/views.py
+++ ginger_arxiv/articles/views.py
@@ -8,6 +8,11 @@ def list_articles(request):
     return render(request, "articles/list.html", {"articles": articles})
 
 
+def author_list(request):
+    authors = Author.objects.all().order_by("name")
+    return render(request, "articles/author_list.html", {"authors": authors})
+
+
 def author_detail(request, author_pk):
     author = get_object_or_404(Author, pk=author_pk)
     return render(request, "articles/author_detail.html", {"author": author})
