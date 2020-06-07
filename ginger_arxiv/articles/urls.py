c8f8c0f28601e4169fdb837ac55f036f9a786637
PJ Hoberman <pj@twoparts.com>
2020-06-06 21:27:45 -0600

    added article detail page and changed links around

--- ginger_arxiv/articles/urls.py
+++ ginger_arxiv/articles/urls.py
@@ -5,6 +5,7 @@ from ginger_arxiv.articles import views
 app_name = "articles"
 urlpatterns = [
     path("", views.list_articles, name="list_articles"),
+    path("article/<int:article_pk>", views.article_detail, name="detail"),
     path("authors", views.author_list, name="author_list"),
     path("authors/<int:author_pk>", views.author_detail, name="author_detail"),
 ]
--- ginger_arxiv/articles/views.py
+++ ginger_arxiv/articles/views.py
@@ -8,6 +8,11 @@ def list_articles(request):
     return render(request, "articles/list.html", {"articles": articles})
 
 
+def article_detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    return render(request, "articles/article_detail.html", {"article": article})
+
+
 def author_list(request):
     authors = Author.objects.all().order_by("name")
     return render(request, "articles/author_list.html", {"authors": authors})
