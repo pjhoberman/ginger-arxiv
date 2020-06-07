0e6ad64bf64d01ba87d497e580e13946013a3925
PJ Hoberman <pj@twoparts.com>
2020-06-07 14:04:38 -0600

    added pagination

--- ginger_arxiv/articles/views.py
+++ ginger_arxiv/articles/views.py
@@ -1,11 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Article, Author
 
 
 def list_articles(request):
-    articles = Article.objects.all()  # todo: check requirements
-    return render(request, "articles/list.html", {"articles": articles})
+    articles = Article.objects.all()
+    paginator = Paginator(articles, 20)
+    page_number = request.GET.get("page")
+    page_articles = paginator.get_page(page_number)
+    return render(request, "articles/list.html", {"articles": page_articles})
 
 
 def article_detail(request, article_pk):
@@ -14,8 +18,11 @@ def article_detail(request, article_pk):
 
 
 def author_list(request):
-    authors = Author.objects.all().order_by("-article_count")[:100]  # todo: paginate
-    return render(request, "articles/author_list.html", {"authors": authors})
+    authors = Author.objects.all().order_by("-article_count")
+    paginator = Paginator(authors, 20)
+    page_number = request.GET.get("page")
+    page_authors = paginator.get_page(page_number)
+    return render(request, "articles/author_list.html", {"authors": page_authors})
 
 
 def author_detail(request, author_pk):
